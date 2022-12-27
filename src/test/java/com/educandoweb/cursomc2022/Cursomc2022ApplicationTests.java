586f1a4f8d55fb7a1a0540a426795be34a6dd042
Andre Leonam <andreleonam@hotmail.com>
2022-12-26 23:33:02 -0300

    Projeto Criado

---
+++ src/test/java/com/educandoweb/cursomc2022/Cursomc2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cursomc2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
