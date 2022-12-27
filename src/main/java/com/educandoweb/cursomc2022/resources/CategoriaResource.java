9d2ca4a9e9cc9b859ac2bb700ac0a029cc061605
Andre Leonam <andreleonam@hotmail.com>
2022-12-27 00:21:17 -0300

    Criando classe Categorias

--- src/main/java/com/educandoweb/cursomc2022/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc2022/resources/CategoriaResource.java
@@ -1,15 +1,33 @@
 package com.educandoweb.cursomc2022.resources;
 
+import com.educandoweb.cursomc2022.entities.Categoria;
+import com.educandoweb.cursomc2022.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST operacional";
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> findAll() {
+        List<Categoria> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Categoria> findById(@PathVariable Long id) {
+        Categoria obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
---
+++ src/main/java/com/educandoweb/cursomc2022/services/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.cursomc2022.services;
+
+import com.educandoweb.cursomc2022.entities.Categoria;
+import com.educandoweb.cursomc2022.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public List<Categoria> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping
+    public Categoria findById(Long id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.get();
+    }
+}
