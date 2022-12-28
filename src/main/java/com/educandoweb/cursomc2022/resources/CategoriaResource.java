28f84c77d3acddeacacf0e04340f6baa350d1436
Andre Leonam <andreleonam@hotmail.com>
2022-12-28 10:40:37 -0300

    Inserindo nova categoria

--- src/main/java/com/educandoweb/cursomc2022/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc2022/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.educandoweb.cursomc2022.entities.Categoria;
 import com.educandoweb.cursomc2022.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,4 +29,23 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+/*
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+*/
 }
--- src/main/java/com/educandoweb/cursomc2022/services/CategoriaService.java
+++ src/main/java/com/educandoweb/cursomc2022/services/CategoriaService.java
@@ -25,4 +25,36 @@ public class CategoriaService {
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public Categoria insert(Categoria obj) {
+        return repository.save(obj);
+    }
+/*
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+             throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setphone(obj.getphone());
+    }
+     */
 }
